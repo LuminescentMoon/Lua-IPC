a8bdfd5bd7b98ddc3ca6174c80ec741eee9a3552
Howard Nguyen <howardf.ho@CLYH-R104-W013.sd36.bc.ca>
2016-02-09 14:21:19 -0800

    Begin consolidation of client and server
    After working on the design of this implementation (documented in
    DESIGN.md in the root folder of the project), I've realized it would be
    a lot simpler to merge the client and server implementations into one
    file as it more closely follows the design goals.

--- src/Main.lua
+++ src/Main.lua
@@ -1,27 +1,19 @@
-local Client, Server
+local LuaSocket, MessagePack
 
 local IPC = {
   _VERSION = '0.0.1-EarlyAccess'
 }
 
-function IPC.client()
-  return Client()
-end
+function IPC:setFilter(port)
 
-function IPC.server()
-  return Server()
 end
 
 return function(currentDir)
   local G = require(currentDir .. '.lib.ev')(currentDir .. '.lib.ev')
 
-  local LuaSocket = require('socket')
-  local MessagePack = require(currentDir .. '.lib.MessagePack')
+  LuaSocket = require('socket')
+  MessagePack = require(currentDir .. '.lib.MessagePack')
 
   local inherits = G.util.inherits
-
-  Client = require(currentDir .. '.src.Client')(inherits, G.EventEmitter, LuaSocket, MessagePack, G.setInterval)
-  Server = require(currentDir .. '.src.Server')(LuaSocket, MessagePack)
-
-  return IPC
+  return inherits(IPC, 'IPC', G.EventEmitter)
 end
